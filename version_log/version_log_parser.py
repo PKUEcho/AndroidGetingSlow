1253a33c9f5ebc0d84e75d5fc7c1db288a160cbf
echo <ec.pku.ho@gmail.com>
2016-06-11 19:07:28 +0800

    something

--- version_log/version_log_parser.py
+++ version_log/version_log_parser.py
@@ -1,8 +1,5 @@
-import re
+import os
 import numpy as np
-
-package_name = 'com.halfbrick.fruitninja'
-log_file = 'raw_log/ninja_5.1.1.log'
 
 def parseLatency(l):
     ll = l.replace('+', '').replace(')', '').replace('ms', '')
@@ -26,82 +23,78 @@ def read_meta_file():
         if line.find('packageName') != -1:
             continue
         items = line.strip().split('\t')
-        if not items[0] in res:
-            res[items[0]] = {}
-        res[items[0]][items[1]] = items[2]
+        res[items[1]] = (items[0], items[2])
     return res
 
 version_map = read_meta_file()
-
 def apk2version(apk):
     if apk.find('apk') != -1:
         apk = apk[:-4]
-    return version_map[package_name][apk]
-
-
-f = open(log_file)
-
-perf = {}
-cur_version = None
-for line in f:
-    # Sample line:
-    # I/PackageManager( 1843 ): Verification timed out for file:///data/local/tmp/fdf8c517ce82db7a684f64d31ee7c329.apk
-    if line.find('Continuing with installation') != -1:
-        print line
-        items = line.strip().split(' ')
-        apk_name = items[-1].strip().split('/')[-1]
-        cur_version = int(apk2version(apk_name))
-        perf[cur_version] = {}
-    # Sample line:
-    # I/ActivityManager( 1843 ): Displayed com.kugou.android/.app.splash.SplashActivity: +667ms
-    if line.find('Displayed') != -1:
-        value = perf[cur_version]
-        items = re.split(' |\t', line.strip())
-        print items
-        activity = (items[4][:-1]).split('.')[-1]
-        if activity not in value:
-            value[activity] = []
-        latency =  parseLatency(items[-1])
-        value[activity].append(latency)
-
-
+    return version_map[apk][1]
 
 # perf format:
-# {v1: {act1: [l1, l2...], act2: [...]},
-#  v2: {...}}
-for version in perf:
-    print version
-    for activity in perf[version]:
-        print '\t' + activity + ': '+ str(len(perf[version][activity]))
+# {package_name/activity: {apk_version: {android_version1: {[l1, l2, ...]}}}}
+perf = {}
+raw_logs = os.listdir('raw_log')
+for log in raw_logs:
+    f = open('raw_log/' + log)
+    android_version = log.strip().split('-')[-1]
+    apk_version = None
+    for line in f:
+        if line.find('Continuing with installation') != -1:
+            items = line.strip().split(' ')
+            apk_name = items[-1].strip().split('/')[-1]
+            apk_version = int(apk2version(apk_name))
+        if line.find('Displayed') != -1:
+            items = line.strip().split(' ')
+            if line.find('(total') != -1:
+                del items[-2]
+                del items[-2]
+            package = (items[-2][:-1]).split('/')[0]
+            activity = (items[-2][:-1]).split('.')[-1]
+            latency =  parseLatency(items[-1])
+            # store in perf
+            key = package + '/' + activity
+            if key not in perf:
+                perf[key] = {}
+            if apk_version not in perf[key]:
+                perf[key][apk_version] = {}
+            if android_version not in perf[key][apk_version]:
+                perf[key][apk_version][android_version] = []
+            perf[key][apk_version][android_version].append(latency)
 
-# Sort via version number
-keys = perf.keys()
-keys.sort()
-sort_perf = [(key, perf[key]) for key in keys]
+interested_android_versions = ['5.1.1', '6.0.0']
+for activity in perf:
+    outFile = open('parse_result/' + activity.replace('/', '-'), 'w+')
+    outFile.write('version\t5.1.1\t6.0.0\t7.0.0\n')
+    cur_perf = perf[activity]
+    keys = cur_perf.keys()
+    keys.sort()
+    sort_perf = [(key, cur_perf[key]) for key in keys]
+    for item in sort_perf:
+        outFile.write(str(item[0]))
 
 activity_name = {
     'com.kugou.android': ['MediaActivity', 'SplashActivity'],
-    'com.youdao.dict': ['DictSplashActivity', 'MainActivity']
+    'com.youdao.dict': ['DictSplashActivity', 'MainActivity'],
+    'com.halfbrick.fruitninja': ['MortarGameActivity'],
+    'com.baidu.video': ['VideoActivity', 'WelcomActivity'],
+    'com.estrongs.android.pop': ['FileExplorerActivity'],
 }
 
-act = activity_name[package_name]
+acts = activity_name[package_name]
 outFile = open('parse_result/' + package_name, 'w+')
-outFile.write('version\t%s\t%s\n' % (act[0], act[1]))
+outFile.write('version\t%s\n' % ('\t'.join(acts)))
 
 for item in sort_perf:
-    outFile.write(str(item[0]) + '\t')
-    if act[0] in item[1]:
-        narray = np.array(item[1][act[0]])
-        outFile.write(str(np.median(narray)) + '\t')
-    else:
-        outFile.write('0\t')
-    if act[1] in item[1]:
-        narray = np.array(item[1][act[1]])
-        outFile.write(str(np.median(narray)) + '\n')
-    else:
-        outFile.write('0\n')
-
-
+    outFile.write(str(item[0]))
+    for act in acts:
+        if act in item[1]:
+            narray = np.array(item[1][act])
+            outFile.write('\t' + str(np.median(narray)))
+        else:
+            outFile.write('\t0')
+    outFile.write('\n')
 
 outFile.close()
 
